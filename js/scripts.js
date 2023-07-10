62d5e7405d78b30d0d018eef035b845e4c4c6a00
leonelvalentinn <leonelvalentinn@gmail.com>
2023-07-10 15:43:42 -0600

    New

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,32 @@ function openNav() {
 function closeNav() {
     document.getElementById("mobile-menu").style.width = "0%";
 }
+
+
+//Script para el slider
+var slideIndex = 0;
+
+showSlides();
+
+function prevSlide (x) {
+    showSlides(slideIndex += x);
+}
+
+function nextSlide (x) {
+    showSlides(slideIndex += x);
+}
+
+function showSlides () {
+    var i;
+    var slide = document.getElementsByClassName("slider-item");
+
+    for (i=0; i<slide.length; i++) {
+        slide[i].style.display = "none";
+    }
+    slideIndex++;
+    
+    if(slideIndex > slide.length) { 
+        slideIndex=1;
+    }
+    slide[slideIndex-1].style.display = "block";
+}
